405405c1f6bc2d13a416755e901f3dad9b451673
CitLi <citlima29@gmail.com>
2025-09-25 10:31:42 +0100

    separated the code in a bit more organized functions and added some log info to the footer

--- src/main.c
+++ src/main.c
@@ -4,74 +4,159 @@
 
 #define KEY_ESC 27
 #define FOOTER_H 4
-int y,x,ch;
+
+enum MODE{
+	INSERT,
+	LIST,
+	NORMAL
+};
+
+int y,x,input;
 WINDOW * main_win;
 WINDOW * footer_win;
+bool quit;
+bool first;
+
+enum MODE mode;
 
 int close(){
-	delwin(main_win);
-	delwin(footer_win);
-	endwin(); 
+	if(main_win!=NULL)
+		delwin(main_win);
+	if(footer_win!=NULL)
+		delwin(footer_win);
+	if(stdscr!=NULL)
+		endwin(); 
 	printf("Caput\n");
 	return 0;
 }
-
-int main()
-{	
-
+int setup(){
+	//variables
 	ESCDELAY = 0;
-	// setup	
-	initscr();
-	cbreak();
-	noecho();
+	mode = NORMAL;
+	quit = false;
+	first = true;
+	//setup windows
+	if(initscr()==NULL) {
+		printf("Error: initscr is NULL\n");
+		return -1;
+	}
+	if(cbreak()==ERR) {
+		printf("Error: cbreak\n");
+		return -1;
+	}
+	if(noecho()==ERR) {
+		printf("Error: noecho\n");
+		return -1;
+	}
 
 	main_win = newwin(LINES-FOOTER_H, COLS, 0, 0);
+	if(main_win ==NULL) {
+		printf("Error: main_win is NULL\n");
+		return -1;
+	}
 	footer_win = newwin(FOOTER_H, COLS, LINES-FOOTER_H, 0);
-	
+	if(footer_win ==NULL) {
+		printf("Error: footer_win is NULL\n");
+		return -1;
+	}
+
 	intrflush(main_win, FALSE);
 	keypad(main_win, TRUE);
 	intrflush(footer_win, FALSE);
 	keypad(footer_win, TRUE);
+	
 	//
-	wmove(footer_win,0,0);
-	whline(footer_win, ACS_HLINE, COLS);
-	wmove(footer_win,1,0);
-	wprintw(footer_win,"This is a footer\n:)\n:o");
-	wrefresh(footer_win);
-	// main loop
-	while(1){
-		ch = wgetch(main_win);
-		wmove(main_win,5,0);
-		wprintw(main_win,"%d LINES: %d\n",ch, LINES);
-		waddch(main_win,ch);
-		switch (ch) {
-			case KEY_ESC: 
-			case 'q':
-			 	goto close;
-				break;
-			case 'j':
-			case KEY_DOWN:
-				if(y<LINES-1-FOOTER_H) y++;
-				break;
-			case 'k':
-			case KEY_UP:
-				if(y>0) y--;
-				break;
-			case 'h':
-			case KEY_LEFT:
-				if(x>0) x--;
-				break;
-			case 'l':
-			case KEY_RIGHT:
-				if(x<(COLS-1)) x++;
-				break;
-		}
+	return 0;
+}
+//proccess inputs
+int process_input_normal(){
 
-		wmove(main_win, y, x);
-		wrefresh(main_win);
+	switch (input) {
+		case KEY_ESC: 
+		case 'q':
+			quit = true;
+			break;
+		case 'j':
+		case KEY_DOWN:
+			if(y<LINES-1-FOOTER_H) y++;
+			break;
+		case 'k':
+		case KEY_UP:
+			if(y>0) y--;
+			break;
+		case 'h':
+		case KEY_LEFT:
+			if(x>0) x--;
+			break;
+		case 'l':
+		case KEY_RIGHT:
+			if(x<(COLS-1)) x++;
+			break;
 	}
 
-close:
+	return 0;
+}
+int process_input(){
+	switch (mode) {
+		case NORMAL:
+			return process_input_normal();
+		break;
+		default:
+		break;
+	
+	}
+	return 0;
+}
+//drawing functions
+void print_log_info(){
+	wmove(footer_win,1,15);
+	wclrtoeol(footer_win);
+	wprintw(footer_win,"%c %d",input, input);
+	wmove(footer_win,2,15);
+	wclrtoeol(footer_win);
+	wprintw(footer_win,"%d|%d",x,y);
+	wmove(footer_win,3,15);
+	wclrtoeol(footer_win);
+	wprintw(footer_win,"%d|%d",LINES,COLS);
+}
+int draw_footer(){
+	//wclear(footer_win);
+	if(first){
+		wmove(footer_win,0,0);
+		whline(footer_win, ACS_HLINE, COLS);
+		wmove(footer_win,1,0);
+		wprintw(footer_win,"Key Press:");
+		wmove(footer_win,2,0);
+		wprintw(footer_win,"x|y:");
+		wmove(footer_win,3,0);
+		wprintw(footer_win,"Lines|Cols:");
+		first = false;
+	}
+	print_log_info();
+	wrefresh(footer_win);
+}
+int draw_main(){
+	curs_set(1);
+	wclear(main_win);
+	wmove(main_win,y,x);
+	wrefresh(main_win);
+}
+int main()
+{	
+
+	if(setup()==-1){
+		close();
+	}
+
+
+	// main loop
+	while(1){
+		draw_footer();
+		draw_main();
+		input = wgetch(main_win);
+		process_input();
+		if(quit) break;
+	}
 	
 	return close();
 }
